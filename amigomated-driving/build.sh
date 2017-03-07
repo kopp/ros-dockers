9ff1a7e639c44b832d1f711d50b40b8c16b52d94
Michael Kopp <kopp.michael@yahoo.de>
2017-03-07 16:48:46 +0100

    amigomated-driving: add --no-cache with explaination to build

--- amigomated-driving/build.sh
+++ amigomated-driving/build.sh
@@ -10,4 +10,12 @@ revision_short=$(echo $revision | head -c 8)
 echo Going to build image from revision $revision_short
 
 # run docker build and tag the image appropriately
-docker build -t amigomated-driving:$revision_short .
+# --no-cache: docker build thinks, that the lines `RUN ...` do the same thing
+#             every time and thus the RUN is not actually executed, but a
+#             cached image is used that was generated from the last time this
+#             RUN was executed.  In this container, a RUN contanins a git
+#             clone, though, which will result in different data fetched and
+#             thus the successive RUNs will create something different.
+docker build --no-cache \
+    -t amigomated-driving:$revision_short \
+    .
